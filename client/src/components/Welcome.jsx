1bc57ba512c6a1a39dae8b55e005e3699f937c44
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-19 00:18:29 +0700

    welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { AiFillPlayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from "./";
 
 const Welcome = () => {
   return (
-    <div>Welcome</div>
+    <div className="flex w-full justify-center items-center">
+
+    </div>
   )
 }
 
