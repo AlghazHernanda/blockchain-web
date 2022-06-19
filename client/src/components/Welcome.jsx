0904782bdf179efd61dab702336437df747975b6
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-19 16:02:30 +0700

    card ethereum

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -27,6 +27,11 @@ const Welcome = () => {
 
   }
 
+  const handleSubmit = () => 
+  {
+
+  }
+
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -105,8 +110,23 @@ const Welcome = () => {
             <Input placeholder="Enter Message" name="message" type="text" 
             //handleChange={handleChange} 
             />
-          </div>
+          
 
+          <div className="h-[1px] w-full bg-gray-400 my-2" />
+
+          {false
+            ? <Loader />
+            : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+              >
+                Send now
+              </button>
+            )}
+
+</div>
 
         </div>
       </div>
