1e823d06d21c65ed93dfdb8e6eb82db10e2a4852
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-21 15:53:27 +0700

    transaction

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -24,17 +24,23 @@ const Welcome = () => {
   const { 
     currentAccount, 
     connectWallet, 
-    // handleChange, 
+    handleChange, 
     // sendTransaction, 
-    // formData, 
+    formData, 
     //isLoading 
   } = useContext(TransactionContext);
   
 
-  const handleSubmit = () => 
-  {
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
 
-  }
+    e.preventDefault();
+
+    if (!addressTo || !amount || !keyword || !message) return;
+
+    sendTransaction();
+  };
+
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -103,16 +109,16 @@ const Welcome = () => {
 
           <div  className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <Input placeholder="Address To" name="addressTo" type="text" 
-            //handleChange={handleChange} 
+            handleChange={handleChange} 
             />
             <Input placeholder="Amount (ETH)" name="amount" type="number" 
-            //handleChange={handleChange} 
+            handleChange={handleChange} 
             />
             <Input placeholder="Keyword (Gif)" name="keyword" type="text" 
-            //handleChange={handleChange} 
+            handleChange={handleChange} 
             />
             <Input placeholder="Enter Message" name="message" type="text" 
-            //handleChange={handleChange} 
+            handleChange={handleChange} 
             />
           
 
--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -17,6 +17,12 @@ const getEthereumContract = () => {
 
 export const TransactionsProvider = ({ children }) => {
     const [currentAccount, setCurrentAccount] = useState("");
+    const [formData, setformData] = useState({ addressTo: "", amount: "", keyword: "", message: "" });
+
+    const handleChange = (e, name) => {
+        setformData((prevState) => ({ ...prevState, [name]: e.target.value }));
+      };
+    
 
     const checkIfWalletIsConnect = async () => {
         try {
@@ -51,6 +57,46 @@ export const TransactionsProvider = ({ children }) => {
         }
       };
 
+      const sendTransaction = async () => {
+        try {
+          if (ethereum) {
+            const { addressTo, amount, keyword, message } = formData;
+            const transactionsContract = createEthereumContract();
+            const parsedAmount = ethers.utils.parseEther(amount);
+    
+            await ethereum.request({
+              method: "eth_sendTransaction",
+              params: [{
+                from: currentAccount,
+                to: addressTo,
+                gas: "0x5208",
+                value: parsedAmount._hex,
+              }],
+            });
+    
+            const transactionHash = await transactionsContract.addToBlockchain(addressTo, parsedAmount, message, keyword);
+    
+            setIsLoading(true);
+            console.log(`Loading - ${transactionHash.hash}`);
+            await transactionHash.wait();
+            console.log(`Success - ${transactionHash.hash}`);
+            setIsLoading(false);
+    
+            const transactionsCount = await transactionsContract.getTransactionCount();
+    
+            setTransactionCount(transactionsCount.toNumber());
+            window.location.reload();
+          } else {
+            console.log("No ethereum object");
+          }
+        } catch (error) {
+          console.log(error);
+    
+          throw new Error("No ethereum object");
+        }
+      };
+    
+
       useEffect(() => {
         checkIfWalletIsConnect();
         //checkIfTransactionsExists();
@@ -66,8 +112,8 @@ export const TransactionsProvider = ({ children }) => {
             currentAccount,
             // isLoading,
             // sendTransaction,
-            // handleChange,
-            // formData,
+            handleChange,
+            formData,
           }}
         >
           {children}
