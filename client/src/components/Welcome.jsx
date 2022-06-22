15b6f7f184ee0c6925c4c1c6015fc39f376cd646
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-23 01:07:22 +0700

    update

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
+import { shortenAddress } from "../utils/shortenAddress";
 import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from "./";
 
@@ -97,8 +97,8 @@ const Welcome = () => {
 
                 <div>
                   <p className="text-white font-light text-sm">
-                    Address
-                    {/* {shortenAddress(currentAccount)} */}
+                    {/* ini function yang di bikin didalem utils */}
+                    {shortenAddress(currentAccount)}
                   </p>
                   <p className="text-white font-semibold text-lg mt-1">
                     Ethereum
