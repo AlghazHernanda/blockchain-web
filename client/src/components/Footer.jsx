2e941bc9ead84b59371d97583967d5bf782a064c
AlghazHernanda <mohamad19015@mail.unpad.ac.id>
2022-06-23 08:33:50 +0700

    giphy

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from "react";
+
+import logo from "../../images/logo.png";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
+      <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
+
+      </div>
+    </div>
   )
 }
 
